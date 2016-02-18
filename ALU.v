dd29244107d5cf66614470eeec092cfbbb359d57
wptalento <wptalento@up.edu.ph>
2016-02-18 15:15:14 +0800

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,12 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: Wilson P Talento
-// 
-// Create Date:    09:31:52 02/18/2015 
-// Design Name: 
-// Module Name:    ALU 
-//////////////////////////////////////////////////////////////////////////////////
 module ALU(
 	input [31:0] opA,
 	input [31:0] opB,
@@ -19,13 +10,22 @@ module ALU(
 	
 	always@(opA or opB or sel) begin	
 		case(sel)
-			3'b000: res = opA + opB;				// ADD
-			3'b001: res = opA - opB;				// SUB
+			3'b000: begin							//ADD
+              res = opA + opB;
+              v = ((opA[31] == opB[31]) && (opA[31] != res[31])) ? 1:0;
+              c = ((opA == 32'hffffffff && opB >= 32'd1) || (opB == 32'hffffffff && opA >= 32'd1)) ? 1:0;
+            end
+			3'b001: begin							//SUB
+              res = opA - opB;
+              v = ((opA[31] == opB[31]) && (opA[31] != res[31])) ? 1:0;
+              c = (opA == 32'h0 && opB >= 32'd1) ? 1:0;
+            end
 			3'b010: res = opA & opB;				// AND
 			3'b011: res = opA | opB;				// OR
 			3'b100: res = ~opA;					    // NOT
 		endcase
-		z = ((opA - opB) == 0) ? 1:0;			// ZERO FLAG
+      z = (res == 0) ? 1:0;
+      
 	end
 
 endmodule
